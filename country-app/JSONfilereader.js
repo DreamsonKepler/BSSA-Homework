3f1e9044fdcfacaa38076f814be290956886819f
Kido Jung <jungkido@gmail.com>
2017-01-27 16:49:01 +0100

    updated version country-app

--- country-app/JSONfilereader.js
+++ country-app/JSONfilereader.js
@@ -7,9 +7,11 @@
 // Part 1: This module should have a function that takes in a filename and a callback function as a parameter. 
 // It should then read that file, then parse the JSON. Finally, it should call the callback function and pass the parsed JSON to it.
 var fs = require ('fs') // Assigned function require with argument 'fs' to var fs
-module.exports = JSONfilereader // Step 1) Created a module: assigned 'JSONfilereader' (is a variable) to module.exports for syntax reasons and to export to another file
 
 
+
+// var JSONfilereader = function (filename, callback)
+// function JSONfilereader (filename, callback)
 var JSONfilereader = function (filename, callback) { // Step 2.1) Created function "JSONfile reader" assigned to the module that takes in a filename a callback function as a a parameter
 	fs.readFile(filename, 'utf-8', function (err, data) { // 2.2) Function reads the file via readfile function
 		if (err) throw err // 2.3) body of readfile function with if (err) throw err
@@ -19,10 +21,7 @@ var JSONfilereader = function (filename, callback) { // Step 2.1) Created functi
 	});
 }
 
-JSONfilereader('./countries.json', function () { //Step 3) Created a callback function passed the parsed JSON to the JSONfilereader function
-	console.log('hallo')
-})
-
+module.exports = JSONfilereader // Step 1) Created a module: assigned 'JSONfilereader' (is a variable) to module.exports for syntax reasons and to export to another file
 
 
 
--- country-app/app.js
+++ country-app/app.js
@@ -9,10 +9,11 @@
 // Part 1) Create a Node.js application that does the following:
 var countryName = process.argv[2] //  step 3.2) Assigns function process.argv to var countryname to clarify syntax
 var fs = require('fs') // step 2.2) Assign require argument fs to var fs
-var JSONfilereader= require('/JSONfilereader')
+var JSONfilereader= require(__dirname + '/JSONfilereader')
 
-JSONfilereader('./countries.json', function () {
+JSONfilereader('./countries.json', function readJSON() { // Call JSONfilereader with arguments countries.json (imports module) and function readJSON
 	console.log('hallo')
+
 })
 
 console.log(process.argv[2]);// step 1) Takes in one parameter from the command line, the name of a country. See set-up Note: command line arguments can be read from the global array process.argv
@@ -22,12 +23,14 @@ fs.readFile('countries.json', 'utf8', function (err, data) { // step 2.1) Connec
 	if (err)
 		throw err
 	var countryinformation = JSON.parse(data); // step 2.3) Assigned the data of countries.json to variable countryinformation. Json.parse accessed the data stored in countries.json and translate this to readible data in javascript.
-	for (var i = 0; i < countryinformation.length; i++) { // step 2.4) Used for loop to go through information in the countries.json file
-		if (countryinformation[i].name === countryName) { // step 2.5) If the information stored in process.argv[2](= countryName) is found in the body of the for loop by countryinformation[i].name -> let me know
-			console.log ('Country:' + countryinformation[i].name); //step 2.6) // Expresses info about the same name found in the countries.json file as the name put to process.argv[2]
-			console.log ('Top level domain:' + countryinformation[i].topLevelDomain); // Expresses info about topLevelDomain which is the same name found in the countries.json file as the name put to process.argv[2]
+	function readJSON () {
+		for (var i = 0; i < countryinformation.length; i++) { // step 2.4) Used for loop to go through information in the countries.json file
+			if (countryinformation[i].name === countryName) { // step 2.5) If the information stored in process.argv[2](= countryName) is found in the body of the for loop by countryinformation[i].name -> let me know
+				console.log ('Country:' + countryinformation[i].name); //step 2.6) // Expresses info about the same name found in the countries.json file as the name put to process.argv[2]
+				console.log ('Top level domain:' + countryinformation[i].topLevelDomain); // Expresses info about topLevelDomain which is the same name found in the countries.json file as the name put to process.argv[2]
 		}
 	}
+}
 });
 
 
